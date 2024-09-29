0dba4f28ddc1e7272c05ce88342bcb55034df360
mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-09-29 23:01:49 +0800

    0929

--- imagek_coil2_test/src/main/java/com/mozhimen/imagek/coil2/test/ImageKCoilActivity.kt
+++ imagek_coil2_test/src/main/java/com/mozhimen/imagek/coil2/test/ImageKCoilActivity.kt
@@ -1,7 +1,7 @@
 package com.mozhimen.imagek.coil2.test
 
 import android.os.Bundle
-import com.mozhimen.mvvmk.bases.activity.viewbinding.BaseActivityVB
+import com.mozhimen.bindk.bases.activity.viewbinding.BaseActivityVB
 import com.mozhimen.imagek.coil2.loadImage_ofCoil
 import com.mozhimen.imagek.coil2.test.databinding.ActivityImagekCoilBinding
 
