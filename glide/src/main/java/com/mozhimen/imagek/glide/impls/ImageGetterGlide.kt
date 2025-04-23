3d10cda095957b849a6fb754fce113e36a9712bc
mozhimen <58215586+mozhimen@users.noreply.github.com>
2025-04-24 00:08:17 +0800

    250424

--- glide/src/main/java/com/mozhimen/imagek/glide/impls/ImageGetterGlide.kt
+++ glide/src/main/java/com/mozhimen/imagek/glide/impls/ImageGetterGlide.kt
@@ -12,6 +12,7 @@ import com.bumptech.glide.request.target.SizeReadyCallback
 import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.mozhimen.kotlin.elemk.commons.IA_AListener
+import com.mozhimen.kotlin.utilk.commons.IUtilK
 import java.lang.ref.WeakReference
 
 
@@ -22,17 +23,21 @@ import java.lang.ref.WeakReference
  * @Date 2025/2/20
  * @Version 1.0
  */
-class ImageGetterGlide(
+open class ImageGetterGlide constructor(
     textView: TextView,
-    private val imageScaleType: ImageScaleType = ImageScaleType.WRAP_CONTENT,
-    private val _modifier: IA_AListener<String>? = null,
-) : ImageGetter {
+    protected val _imageScaleType: ImageScaleType = ImageScaleType.WRAP_CONTENT,
+    protected val _modifier: IA_AListener<String>? = null,
+) : ImageGetter, IUtilK {
     enum class ImageScaleType {
         WRAP_CONTENT,
         MATCH_PARENT
     }
 
-    private val _textViewRef: WeakReference<TextView> = WeakReference(textView)
+    ////////////////////////////////////////////////////////////////////////////
+
+    protected val _textViewRef: WeakReference<TextView> = WeakReference(textView)
+
+    ////////////////////////////////////////////////////////////////////////////
 
     override fun getDrawable(source: String): Drawable {
         val finalSource = _modifier?.invoke(source) ?: source
@@ -53,16 +58,19 @@ class ImageGetterGlide(
         return bitmapDrawablePlaceholder
     }
 
-    private inner class BitmapDrawablePlaceholder : BitmapDrawable(_textViewRef.get()?.resources, Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)), Target<Bitmap> {
+    ////////////////////////////////////////////////////////////////////////////
 
-        private var drawable: Drawable? = null
+    inner class BitmapDrawablePlaceholder : BitmapDrawable(_textViewRef.get()?.resources, Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)), Target<Bitmap> {
+
+        private var _drawable: Drawable? = null
             set(value) {
                 field = value
+                val textView = _textViewRef.get() ?: return
                 value?.let { drawable ->
                     val drawableWidth = (drawable.intrinsicWidth)
                     val drawableHeight = (drawable.intrinsicHeight)
-                    val maxWidth = _textViewRef.get()!!.measuredWidth
-                    if (drawableWidth > maxWidth || imageScaleType == ImageScaleType.MATCH_PARENT) {
+                    val maxWidth = textView.measuredWidth
+                    if (drawableWidth > maxWidth || _imageScaleType == ImageScaleType.MATCH_PARENT) {
                         val calculatedHeight = maxWidth * drawableHeight / drawableWidth
                         drawable.setBounds(0, 0, maxWidth, calculatedHeight)
                         setBounds(0, 0, maxWidth, calculatedHeight)
@@ -70,33 +78,39 @@ class ImageGetterGlide(
                         drawable.setBounds(0, 0, drawableWidth, drawableHeight)
                         setBounds(0, 0, drawableWidth, drawableHeight)
                     }
-                    _textViewRef.get()?.text = _textViewRef.get()?.text
+                    textView.post {
+                        _textViewRef.get()?.text = _textViewRef.get()?.text
+                    }
                 }
             }
 
+        ////////////////////////////////////////////////////////////////////////////
+
         override fun draw(canvas: Canvas) {
-            drawable?.draw(canvas)
+            _drawable?.draw(canvas)
         }
 
         override fun onLoadStarted(placeholder: Drawable?) {
             placeholder?.let {
-                drawable = it
+                _drawable = it
             }
         }
 
         override fun onLoadFailed(errorDrawable: Drawable?) {
             errorDrawable?.let {
-                drawable = it
+                _drawable = it
             }
         }
 
         override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-            drawable = BitmapDrawable(_textViewRef.get()!!.resources, bitmap)
+            _textViewRef.get()?.let { textView ->
+                _drawable = BitmapDrawable(textView.resources, resource)
+            }
         }
 
         override fun onLoadCleared(placeholder: Drawable?) {
             placeholder?.let {
-                drawable = it
+                _drawable = it
             }
         }
 
